ed9bae6d803af6978fe902bbdf578d9f1b675f0a
Ahmed Khogali <ahmed.khog@outlook.com>
2023-07-16 13:59:15 +0300

    added a list of players for the teams

--- python-team-rand.py
+++ python-team-rand.py
@@ -1,1 +1,16 @@
+import random
 
+print("Build your own random sport team!")
+
+first_names = [
+    # Male Names
+    "Liam", "Noah", "Oliver", "Ethan", "Aiden", "Mason", "Caden", "Jackson", "Lucas", "Logan",
+    "Elijah", "Michael", "Alexander", "James", "William", "Benjamin", "Daniel", "Henry", "Joseph", "Matthew",
+
+    # Female Names
+    "Olivia", "Emma", "Ava", "Sophia", "Isabella", "Mia", "Amelia", "Harper", "Evelyn", "Abigail",
+    "Emily", "Elizabeth", "Sofia", "Ella", "Scarlett", "Grace", "Chloe", "Victoria", "Zoe", "Hannah"
+]
+
+print("Time to randomize the 2 teams....")
+random.shuffle(first_names)
