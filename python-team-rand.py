b73dcfe43b317677d8361a9a26070e43d062ab2a
Ahmed Khogali <ahmed.khog@outlook.com>
2023-07-16 14:18:56 +0300

    finished initial setup for the first team

--- python-team-rand.py
+++ python-team-rand.py
@@ -2,7 +2,7 @@ import random
 
 print("Build your own random sport team!")
 
-first_names = [
+names = [
     # Male Names
     "Liam", "Noah", "Oliver", "Ethan", "Aiden", "Mason", "Caden", "Jackson", "Lucas", "Logan",
     "Elijah", "Michael", "Alexander", "James", "William", "Benjamin", "Daniel", "Henry", "Joseph", "Matthew",
@@ -12,5 +12,20 @@ first_names = [
     "Emily", "Elizabeth", "Sofia", "Ella", "Scarlett", "Grace", "Chloe", "Victoria", "Zoe", "Hannah"
 ]
 
-print("Time to randomize the 2 teams....")
-random.shuffle(first_names)
+print("Time to randomize the teams....")
+random.shuffle(names)
+
+#spliting the randomized list to 2 teams.....starting with team1
+first_team = names[:len(names)//2]
+print("")
+
+#picking a captain randomly for the team
+team1_cap = random.choice(first_team)
+print("Team 1's Captain is:", team1_cap)
+
+#dispalying the first team:
+print("")
+print("Team 1:")
+for member in first_team:
+    print(member)
+print("")
